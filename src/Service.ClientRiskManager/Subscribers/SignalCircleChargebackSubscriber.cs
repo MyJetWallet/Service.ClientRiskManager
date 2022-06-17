ab6e0991323b41b0ea8a8ee35c1d1fae657be1cc
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-06-17 11:54:39 +0400

    https://smplt.atlassian.net/browse/SPBL-254

--- src/Service.ClientRiskManager/Subscribers/SignalCircleChargebackSubscriber.cs
+++ src/Service.ClientRiskManager/Subscribers/SignalCircleChargebackSubscriber.cs
@@ -4,6 +4,8 @@ using MyJetWallet.Sdk.Service;
 using Service.Circle.Webhooks.Domain.Models;
 using Service.ClientProfile.Grpc;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Service.ClientRiskManager.Subscribers
@@ -13,6 +15,7 @@ namespace Service.ClientRiskManager.Subscribers
     {
         private readonly ILogger<SignalCircleChargebackSubscriber> _logger;
         private readonly IClientProfileService _clientProfileService;
+        private readonly SemaphoreSlim _locker = new SemaphoreSlim(1);
 
         public SignalCircleChargebackSubscriber(
             ILogger<SignalCircleChargebackSubscriber> logger,
@@ -32,38 +35,57 @@ namespace Service.ClientRiskManager.Subscribers
 
             try
             {
+                await _locker.WaitAsync();
                 var reason = $"CircleChargeback: {signal.Chargeback.Id}";
                 var expiryTime = DateTime.UtcNow + TimeSpan.FromDays(365);
 
-                await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
+                var blockers = _clientProfileService.GetClientBlockers(new ClientProfile.Grpc.Models.Requests.Blockers.GetClientProfileBlockersRequest
                 {
-                    BlockerReason = reason,
-                    ClientId = signal.ClientId,
-                    ExpiryTime = expiryTime,
-                    Type = ClientProfile.Domain.Models.BlockingType.Withdrawal,
                 });
 
-                await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
-                {
-                    BlockerReason = reason,
-                    ClientId = signal.ClientId,
-                    ExpiryTime = expiryTime,
-                    Type = ClientProfile.Domain.Models.BlockingType.Transfer,
-                });
+                var hashSet = new HashSet<ClientProfile.Domain.Models.BlockingType>();
 
-                await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
+                await foreach (var item in blockers)
                 {
-                    BlockerReason = reason,
-                    ClientId = signal.ClientId,
-                    ExpiryTime = expiryTime,
-                    Type = ClientProfile.Domain.Models.BlockingType.Trade,
-                });
+                    hashSet.Add(item.BlockedOperationType);
+                }
+
+                if (!hashSet.Contains(ClientProfile.Domain.Models.BlockingType.Withdrawal))
+                    await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
+                    {
+                        BlockerReason = reason,
+                        ClientId = signal.ClientId,
+                        ExpiryTime = expiryTime,
+                        Type = ClientProfile.Domain.Models.BlockingType.Withdrawal,
+                    });
+
+                if (!hashSet.Contains(ClientProfile.Domain.Models.BlockingType.Transfer))
+                    await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
+                    {
+                        BlockerReason = reason,
+                        ClientId = signal.ClientId,
+                        ExpiryTime = expiryTime,
+                        Type = ClientProfile.Domain.Models.BlockingType.Transfer,
+                    });
+
+                if (!hashSet.Contains(ClientProfile.Domain.Models.BlockingType.Trade))
+                    await _clientProfileService.AddBlockerToClient(new ClientProfile.Grpc.Models.Requests.AddBlockerToClientRequest
+                    {
+                        BlockerReason = reason,
+                        ClientId = signal.ClientId,
+                        ExpiryTime = expiryTime,
+                        Type = ClientProfile.Domain.Models.BlockingType.Trade,
+                    });
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error processing SignalCircleChargeback {@context}", signal.ToJson());
                 ex.FailActivity();
                 throw;
+            }
+            finally
+            {
+                _locker.Release();
             }
         }
     }
